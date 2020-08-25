760805e15397eea63516ebc7d213c725e8a2533d
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-08-24 20:21:50 -0500

    Create test to verify User object carries data properties

--- test/User-Test.js
+++ test/User-Test.js
@@ -6,7 +6,7 @@ let testUser
 
 describe('User', () => {
   beforeEach((done) => {
-    testUser = new User;
+    testUser = new User(15,'Ezequiel Feest', '78801 Lauryn Plain, Lake Elinor MN 27856-9054', 'Anthony_Toy@hotmail.com', 4.4, 5000, [33, 14, 3, 43, 35]);
   done();
   })
 
@@ -17,4 +17,8 @@ describe('User', () => {
   it('should be an instance of User', () => {
     expect(testUser).to.be.an.instanceof(User)
   });
+
+  it('should create a parameter for each users data', () => {
+    expect(testUser.id).to.equal(15)
+  })
 });
