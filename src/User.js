82d83d4063b9538e9f9948f030c43195a40bc875
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-25 16:38:06 -0600

    Remove vestigial commented out code

--- src/User.js
+++ src/User.js
@@ -10,8 +10,6 @@ class User {
     this.friends = data.friends;
   }
   displayFirstName() {
-    // take this.name and nix the last name
-    // use the space as an indicator
     let nameArray = this.name.split(' ')
     let firstNameOnlyArray = nameArray.splice(0, 1)
     let firstName = firstNameOnlyArray.toString()
@@ -19,23 +17,6 @@ class User {
   }
 }
 
-/*
-{
-  "id": 15,
-  "name": "Ezequiel Feest",
-  "address": "78801 Lauryn Plain, Lake Elinor MN 27856-9054",
-  "email": "Anthony_Toy@hotmail.com",
-  "strideLength": 4.4,
-  "dailyStepGoal": 5000,
-  "friends": [
-    10,
-    23,
-    35,
-    20,
-    5
-  ]
-},
-*/
 if (typeof module !== 'undefined') {
   module.exports = User;
 } else {
