7d2bf63cd63efbb929453dcc579b18c06d419ad3
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-26 16:54:25 -0600

    Add constructor function and initial functionality test

---
+++ src/HydrationRepository.js
@@ -0,0 +1,10 @@
+class HydrationRepository {
+  constructor(data){
+    this.data = data
+    }
+  }
+  if (typeof module !== 'undefined') {
+    module.exports = HydrationRepository;
+  } else {
+    module.exports = HydrationRepository;
+  }
