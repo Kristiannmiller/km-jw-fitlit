0fb54f02d8cc2917a4826c5e9340eec3d97f7a9b
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-27 13:35:02 -0600

    Add test asserting that findUserById method will return a User object

--- test/HydrationRepository-Test.js
+++ test/HydrationRepository-Test.js
@@ -43,12 +43,12 @@ describe('HydrationRepository', () => {
     expect(testRepository.data[2].userID).to.equal(3)
     expect(testRepository.data[3].userID).to.equal(4)
   })
-  //
-  // it('should be able to access user data by id', () => {
-  //   let testRepository = new HydrationRepository(userData);
-  //   expect(testRepository.findUserById(15)).to.deep.equal(userData[0])
-  // })
-  //
+
+  it('should be able to access user data by id', () => {
+    let testRepository = new HydrationRepository(userData);
+    expect(testRepository.findUserById(4)).to.deep.equal(userData[4])
+  })
+
   // it('should be able to return the average step goal amongst all users', () => {
   //   let testRepository = new HydrationRepository(userData);
   //   expect(testRepository.calculateAverageStepGoal()).to.equal(7250)
