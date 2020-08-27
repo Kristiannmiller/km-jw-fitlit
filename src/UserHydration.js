a5c185ab73437c6cfcc7af11cb6eefd6e3bffe78
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-27 14:33:55 -0600

    Add test to verify that UserHydration is a function

--- src/UserHydration.js
+++ src/UserHydration.js
@@ -1,7 +1,12 @@
 class UserHydration {
-  constructor(data){
-    this.id = data.id,
-    this.date = data.name,
-    this.numOunces = data.numOunces
+  constructor(userWaterIntake){
+    // this.id = data.id,
+    // this.date = data.name,
+    // this.numOunces = data.numOunces
   }
+}
+if (typeof module !== 'undefined') {
+  module.exports = UserHydration;
+} else {
+  module.exports = UserHydration;
 }
--- test/UserHydration-Test.js
+++ test/UserHydration-Test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const UserHydration = require('../src/UserHydration')
+const HydrationRepository = require('../src/HydrationRepository')
 const userData = [
   {
     "userID": 1,
@@ -22,6 +23,26 @@ const userData = [
     "userID": 4,
     "date": "2019/06/15",
     "numOunces": 85
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "numOunces": 20
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "numOunces": 25
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "numOunces": 75
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/16",
+    "numOunces": 100
   }
 ];
 
