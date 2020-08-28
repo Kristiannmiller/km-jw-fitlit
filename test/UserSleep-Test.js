8d98a711cc20ec7bfd9e52f75be4f417decdddd6
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-08-27 19:55:56 -0500

    Add sample week dataset for UserSleep-Test.js

--- test/UserSleep-Test.js
+++ test/UserSleep-Test.js
@@ -28,7 +28,56 @@ const sleepData = [
     "hoursSlept": 5.4,
     "sleepQuality": 3
   }
-];
+  {
+    "userID": 4,
+    "date": "2019/06/16",
+    "hoursSlept": 4.1,
+    "sleepQuality": 4.2
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/17",
+    "hoursSlept": 4.1,
+    "sleepQuality": 2.2
+  },
+
+  {
+    "userID": 4,
+    "date": "2019/06/18",
+    "hoursSlept": 7.1,
+    "sleepQuality": 5
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/19",
+    "hoursSlept": 6.1,
+    "sleepQuality": 4.2
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/20",
+    "hoursSlept": 14,
+    "sleepQuality": 1.2
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/21",
+    "hoursSlept": 5.5,
+    "sleepQuality": 3.2
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/22",
+    "hoursSlept": 5.0,
+    "sleepQuality": 2.6
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/23",
+    "hoursSlept": 9.1,
+    "sleepQuality": 4.0
+  },
+  ];
 
 describe('UserSleep', () => {
 
@@ -40,5 +89,12 @@ describe('UserSleep', () => {
     let userSleepStats = new UserSleep()
     expect(userSleepStats).to.be.an.instanceof(UserSleep)
   });
-  
+
+  it ('should return number of hours slept for a specific user on a specific date', () => {
+    let sleepRepository = new SleepRepository(userSleepData)
+    userSleepStats = sleepRepository.findUserById(4)
+    let userSleepHours = new UserSleep(userSleepStats)
+
+    expect(userSleepstats.claculateAverageSleepHours()).to.equal
+  })
 })
