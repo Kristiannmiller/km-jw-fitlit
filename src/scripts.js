a21f70331ba7fea755f7a9768536b9b4f728ca52
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-29 15:06:59 -0600

    Add basic variable and function organization

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+// ********** GLOBAL VARIABLE **********
+var currentUser = {};
+
+// ********** QUERIES  **********
+const profilePage = document.getElementById('#profileName');
+const sideBar = document.querySelector('.box d');
+
+// ********** EVENT LISTENERS **********
+profilePage.addEventListener('click', displayProfile);
+
+
+// ******** FUNCTIONS/EVENTHANDLERS **********
+//create an info card on the dash with all the users info on the page
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function accessProfile() {
+
+}
+
+function displayProfile() {
+  sideBar.innerHTML = ''
+  sideBar.innerHTML =
+  ``
+  //get user info
+  //interpolate user info into profile
+}
