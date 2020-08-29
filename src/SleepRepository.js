00a787537e38c56ee3bc83755da554be63a556c2
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-08-28 19:33:30 -0500

    Add function to select week of data to be analyzed

--- src/SleepRepository.js
+++ src/SleepRepository.js
@@ -6,16 +6,42 @@ class SleepRepository {
       let userSleepData = this.allUserData.filter(userData => userData.userID === id)
       return userSleepData
     }
-    getSleepQualityAverage (){
-    let averageSleepQuality = this.allUserData.reduce((acc, user) => {
-      acc += user.sleepQuality
-    return acc
-  }, 0 )/this.allUserData.length;
-    var result = averageSleepQuality.toFixed(2)
-    return parseFloat(result)
-  }
-}
+    selectWeek(endingDate){
+      let endingDateIndex =
+      this.allUserData.findIndex(sleepData => sleepData.date === endingDate)
+      let weekOfSleep = this.allUserData.slice(endingDateIndex-6, endingDateIndex+1)
+      console.log(weekOfSleep)
+      return weekOfSleep
+    }
+    getSleepQualityAverage(){
+      let averageSleepQuality = this.allUserData.reduce((acc, user) => {
+        acc += user.sleepQuality
+        return acc
+      }, 0 )/this.allUserData.length;
+      let result = averageSleepQuality.toFixed(2)
+      return parseFloat(result)
+    }
+    getAverageWeeklySleepQualityForUser(id, endingDate){
+      selectWeek(endingDate)
+      let averageSleepQualityForUser = this.allUserData.id.reduce((acc, user) => {
+        acc += user.sleepQuality
+        return acc
+      }, 0)/7
+    }
+    findGoodSleepers (endingDate){
+      this.selectWeek('2019/06/23')
+      const currentUser = this.allUserData.findUserById()
+      this.allUserData.findUserById()
+        /7
+    //the sleepData contains an array of objects with all the users sleep information, including ID, and sleep quality, on any given night
+    //we want to  create an array of objects with 1 key value pair: userID, and sleepQuality average for a given week
 
+    //1.we want to start by selecting the week
+    //2. we want to return a shallow copy of the sleepData array that returns all userObjects for that week
+    //3. We will assign this array to a variable.
+    //4. we will use filter over this variable to return a shallower array of only the users whos Average sleepquality value is greater than 3.
+}
+}
   if (typeof module !== 'undefined') {
     module.exports = SleepRepository;
   }
--- test/SleepRepository-Test.js
+++ test/SleepRepository-Test.js
@@ -26,6 +26,55 @@ const sleepData = [
     "date": "2019/06/15",
     "hoursSlept": 5.4,
     "sleepQuality": 3
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/16",
+    "hoursSlept": 4.1,
+    "sleepQuality": 4.2
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/17",
+    "hoursSlept": 4.1,
+    "sleepQuality": 2.2
+  },
+
+  {
+    "userID": 4,
+    "date": "2019/06/18",
+    "hoursSlept": 7.1,
+    "sleepQuality": 5
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/19",
+    "hoursSlept": 6.1,
+    "sleepQuality": 4.2
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/20",
+    "hoursSlept": 14,
+    "sleepQuality": 1.2
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/21",
+    "hoursSlept": 5.5,
+    "sleepQuality": 3.2
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/22",
+    "hoursSlept": 5.0,
+    "sleepQuality": 2.6
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/23",
+    "hoursSlept": 9.1,
+    "sleepQuality": 4.0
   }
 ];
 
@@ -50,8 +99,10 @@ describe('SleepRepository', () => {
 
   it('should calculate the average sleep quality for all users', () => {
     let testRepository = new SleepRepository(sleepData);
-
-    expect(testRepository.getSleepQualityAverage()).to.equal(3.65)
-
+    expect(testRepository.getSleepQualityAverage()).to.equal(3.43)
 });
+  it('should find all users who average a sleep quality greater than three for a given week', () => {
+    let testRepository = new SleepRepository(sleepData);
+    expect(testRepository.findGoodSleepers()).to.deep.equal([2,3,4])
+  });
 });
