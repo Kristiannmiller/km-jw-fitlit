9bcb79aa8badf1d6cf7cc3e02159e8e6d59a304c
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-29 21:17:47 -0600

    Display team mates associated with user on the dashboard with displayTeamList function

--- src/scripts.js
+++ src/scripts.js
@@ -5,14 +5,14 @@ var currentUserRepository
 // ********** QUERIES  **********
 const userName = document.querySelector('.username');
 const myProfileLink = document.querySelector('.profileLink');
-const sideBarContents = document.querySelector('.sideBarContents');
+const sideBarContents = document.getElementById('sidebarContents');
 const userPhoto = document.getElementById('userPhoto')
 const userSteps = document.getElementById('userSteps')
 
 // ********** EVENT LISTENERS **********
-window.addEventListener("load", updateUser);
+window.addEventListener('load', updateUser);
 userName.addEventListener('click', displayProfile);
-myProfileLink.addEventListener('click', displayProfile);
+// myProfileLink.addEventListener('click', displayProfile);
 
 // ******** FUNCTIONS/EVENTHANDLERS **********
 //create an info card on the dash with all the users info on the page
@@ -23,29 +23,48 @@ function getRandomIndex(array) {
 function updateUser() {
   uploadData()
   displayUserData()
-  // displayTeamList()
+  displayTeamList()
 }
+
 function uploadData() {
   currentUser = new User(userData[getRandomIndex(userData)])
   currentUserTeam = currentUser.friends.map(friend => userData[friend-1])
-  currentUserTeam.unshift(currentUser)
+  currentUserTeam.unshift(currentUser) // NOTE: currentUser will always be at [0]
+  // WE CAN CHANGE THIS IF WE NEED TO ^
   currentUserRepository = new UserRepository(currentUserTeam)
 }
 
 function displayUserData() {
   userName.innerText = `${currentUser.name}`
-  userSteps.innerText = ``//***update when we do activity**
+  userSteps.innerText = ``// ***update when we do activity**
   userPhoto.innerHTML = '<img class="userProfilePhoto" src="../assets/userImages/UserImage.jpg" alt="user profile image">'
 }
 
 function displayTeamList() {
-
+  sideBarContents.innerHTML = '<div class="teamHeader">My Team</div>'
+  currentUserRepository.data.forEach((friend, i) => {
+    if (i > 0) {
+      sideBarContents.innerHTML +=
+      `<div class="teammember">
+      <div class="photo sidebar"><img class="teamImage" src="../assets/userImages/TeamImage${i}.jpg" alt="teamMate${i} profile image"></div>
+      <div class="name sidebar">${friend.name}</div>
+      <div class="steps sidebar">${friend.dailyStepGoal}</div>
+      </div>`
+    } i++
+  })
 }
 
 function displayProfile() {
   sideBarContents.innerHTML = ''
   sideBarContents.innerHTML =
-  ``
-  //get user info
-  //interpolate user info into profile
+  `<div class="sidebarHeader">
+    <img class="profileUserPhoto" src="../assets/userImages/UserImage.jpg" alt="user profile image"></div>
+    <div class="profileUser">${currentUser.name}</div>
+    <div class="key profileName">NAME: ${currentUser.name}</div>
+    <div class="key profileAddress">ADDRESS: ${currentUser.address}</div>
+    <div class="key profileEmail">EMAIL: ${currentUser.email}</div>
+    <div class="key profileStride">STRIDE LENGTH: ${currentUser.strideLength}</div>
+    <div class="key profileStepGoal">DAILY STEP GOAL: ${currentUser.dailyStepGoal}</div>
+    <div class="viewTeam" type="button">View My Team</div>
+  </div>`
 }
