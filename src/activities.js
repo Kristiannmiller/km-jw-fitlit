a6d947fc5e5ea7596431e42762e538c1c9995ba2
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-08-31 17:45:40 -0500

    Build calculateAverageMinutesActivePerWeek function

--- src/activities.js
+++ src/activities.js
@@ -9,7 +9,13 @@ class UserActivity {
 
   }
   calculateAvgMinActivePerWeek(endingDate){
-
+    let endingDateIndex = this.userActivity.findIndex(activityData => activityData.date === endingDate)
+    let weekOfActivity = this.userActivity.slice(endingDateIndex-6, endingDateIndex+1)
+    let result = weekOfActitivty.reduce((activityData, day) => {
+      activityData[day.date] = day.minutesActive
+      return activityData
+    }, {})
+    return result
   }
   calculateStepGoalAchieved(date){
 
@@ -18,6 +24,6 @@ class UserActivity {
 
   }
   calculateMostStairsClimbed(user, date){
-    
+
   }
 }
