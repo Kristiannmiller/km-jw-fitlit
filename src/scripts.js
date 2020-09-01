498a977a82cb9c0e964af92ac4f041e2c8c1fe57
Kristi Miller <kristi.ann.miller@gmail.com>
2020-08-31 20:46:42 -0600

    Display weekly sleep quality over course of week to dashboard

--- src/scripts.js
+++ src/scripts.js
@@ -322,3 +322,83 @@ function displayDailySleepQualityChart(currentUserSleep, allUserSleepRepository)
       }
   });
 };
+function displayWeeklySleepHoursChart(currentUserSleep, allUserSleepRepository) {
+  let currentWeek = generateWeekDates()
+  let sleepHrsWeek = document.getElementById('weeklySleepHoursChart');
+  let sleepHoursChartWeek = new Chart(sleepHrsWeek, {
+    type: 'horizontalBar',
+    data: {
+      labels: currentWeek,
+      datasets: [{
+        label: 'Hours Slept',
+        data: currentUserSleep.calculateDailySleepHoursForWeek(weekEnd),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          }
+        }]
+      }
+    }
+  })
+}
+function displayWeeklySleepQualityChart(currentUserSleep, allUserSleepRepository) {
+  let currentWeek = generateWeekDates()
+  let sleepQualWeek = document.getElementById('weeklySleepQualityChart');
+  let sleepQualChartWeek = new Chart(sleepQualWeek, {
+    type: 'horizontalBar',
+    data: {
+      labels: currentWeek,
+      datasets: [{
+        label: 'Quality of Sleep',
+        data: currentUserSleep.calculateDailySleepQualityForWeek(weekEnd),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          }
+        }]
+      }
+    }
+  })
+}
